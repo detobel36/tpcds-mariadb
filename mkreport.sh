c96b391de0f37754491ad809881b4f41ba3d3d01
R Detobel <rdetobel@ulb.ac.be>
2018-10-29 09:53:45 +0100

    Use longtable and not table for LaTeX report

--- mkreport.sh
+++ mkreport.sh
@@ -31,7 +31,8 @@ then
 elif [ "$TYPE_REPORT" = "latex" ]
 then
 	OUTPUT_RESULT_FILE="result.tex"
-	printf "\\\\begin{table}[]\n\\\\begin{tabular}{|l|l|l|l|}\n\\\\hline\nQuery & Template & Time (secs) & Note \\\\\\ \\hline\n" > $OUTPUT_RESULT_FILE
+	printf "\\\\begin{longtable}{|c|c|c|c|}\n\\\\hline\n Query & Template & Time (secs) & Note \\\\\\ \\\\hline\n\\\\endfirsthead\n\\\\endhead\n\\\\endfoot\\\\endlastfoot" > $OUTPUT_RESULT_FILE
+	#printf "\\\\begin{table}[]\n\\\\begin{tabular}{|l|l|l|l|}\n\\\\hline\nQuery & Template & Time (secs) & Note \\\\\\ \\hline\n" > $OUTPUT_RESULT_FILE
 
 	function generateLine {
 		LIGNE=$(echo $1 | sed -r 's/\|/ \& /g')
@@ -39,7 +40,8 @@ then
 	}
 
 	function endFile {
-		printf "\\\\end{tabular}\n\\\\end{table}"
+		printf "\\\\end{longtable}"
+		# printf "\\\\end{tabular}\n\\\\end{table}"
 	}
 
 
